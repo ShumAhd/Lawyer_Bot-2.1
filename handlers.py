7010de103166c740166ab3cb623cd9cf8cdbda21
Andres <106507320+ShumAhd@users.noreply.github.com>
2024-09-06 10:44:24 +0800

    Update handlers.py
    Исправлена ошибка, бот не запускался когда пользователь добавлял его к себе на прямую миную общий чат.

--- handlers.py
+++ handlers.py
@@ -25,36 +25,27 @@ class Form(StatesGroup):
     waiting_for_question = State()
     waiting_for_phone = State()
 
-# Функция-заглушка для проверки релевантности топика
+# Функция для проверки релевантности топика
 def is_relevant_topic(message: Message) -> bool:
-    # Проверяем, если это личный чат или нужная тема
     return (
-        message.chat.type == "private" or  # Условие для личных сообщений
-        (message.is_topic_message
-        and message.message_thread_id is not None
-        and int(message.message_thread_id) == int(TOPIC_ID))
+        message.chat.type == "private" or
+        (message.is_topic_message and message.message_thread_id is not None and int(message.message_thread_id) == int(TOPIC_ID))
     )
 
-
-
-# Обработчики сообщений
-
-def is_question_to_lawyer(message: Message) -> bool:
-    return (
-        message.text == "Вопрос юристу" and
-        is_relevant_topic(message)
-    )
-
-@router.message(lambda message: message.text == "Вопрос юристу" and is_relevant_topic(message))
-async def text_handler(message: Message):
-    bot_info = await bot.get_me()
-    bot_username = bot_info.username
-
-    # Проверяем, является ли это сообщение из личного чата
+@router.message(CommandStart())
+async def start_handler(message: Message, state: FSMContext):
     if message.chat.type == "private":
-        await ask_question(message)
-    else:
-        # Если сообщение не из личного чата, отправляем пользователя в личный чат с ботом
+        await ask_question(message, state)
+    elif is_relevant_topic(message):
+        await message.answer("Для того чтобы задать вопрос юристу РО, просто напишите 'Вопрос юристу'")
+
+@router.message(lambda message: message.text.lower() == "вопрос юристу")
+async def handle_question_to_lawyer(message: Message, state: FSMContext):
+    if message.chat.type == "private":
+        await ask_question(message, state)
+    elif is_relevant_topic(message):
+        bot_info = await bot.get_me()
+        bot_username = bot_info.username
         button = types.InlineKeyboardButton(
             text='Перейти в личные сообщения',
             url=f'https://t.me/{bot_username}?start=ask'
@@ -62,19 +53,6 @@ async def text_handler(message: Message):
         markup = types.InlineKeyboardMarkup(inline_keyboard=[[button]])
         await message.answer("Чтобы задать вопрос юристу, перейдите в личные сообщения.", reply_markup=markup)
 
-
-@router.message(CommandStart(), lambda message: is_relevant_topic(message))
-async def start_handler(message: Message, state: FSMContext):
-    # Получаем текст команды
-    command_args = message.text.split(maxsplit=1)
-
-    # Если есть аргументы (например, "ask")
-    if len(command_args) > 1 and command_args[1] == "ask":
-        await ask_question(message, state)
-    else:
-        await message.answer("Чтобы задать вопрос юристу РО, просто напишите Вопрос юристу")
-
-# Функция для запроса вопроса пользователя
 async def ask_question(message: Message, state: FSMContext):
     await message.answer(
         'Задайте себе несколько вопросов:\n'
@@ -97,33 +75,25 @@ async def process_question_step(message: Message, state: FSMContext):
     await message.answer('Для продолжения введите номер своего телефона в международном формате, пример: +79241233223')
     await state.set_state(Form.waiting_for_phone)
 
-
-# Настройка базового конфигурирования логирования
-logging.basicConfig(level=logging.INFO)
-
-
 @router.message(Form.waiting_for_phone)
 async def process_phone_step(message: Message, state: FSMContext):
-    logging.info("Processing phone step")  # Логирование начала функции
+    logging.info("Processing phone step")
 
     if re.match(r"^\+\d{11}$", message.text):
         user_data = await state.get_data()
         question = user_data['question']
 
         await bot.send_message(
-            TARGET_CHAT_ID,  # Отправка сообщения в чат TARGET_CHAT_ID
+            TARGET_CHAT_ID,
             f'Новый вопрос от {message.from_user.first_name} @{message.from_user.username}\n\n'
             f'Вопрос: {question}\nКонтактный номер: {message.text}'
         )
 
-        logging.info("About to create keyboard")  # Лог перед созданием клавиатуры
-        # Создание клавиатуры
         markup = types.ReplyKeyboardMarkup(
             keyboard=[[types.KeyboardButton(text='Задать новый вопрос юристу')]],
             resize_keyboard=True,
             one_time_keyboard=True
         )
-        logging.info("Keyboard created successfully")  # Лог после успешного создания клавиатуры
 
         await message.answer(
             'Спасибо! Ваш вопрос отправлен. С вами свяжутся юристы.',
@@ -140,8 +110,7 @@ async def process_phone_step(message: Message, state: FSMContext):
 async def new_question_handler(message: Message, state: FSMContext):
     await ask_question(message, state)
 
-
 # Подключение роутера к диспетчеру
 dp.include_router(router)
 
-__all__ = ['dp', 'bot']
+all = ['dp', 'bot']
